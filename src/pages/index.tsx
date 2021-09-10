d16787f844b707fa21e3007530a7acbfede556cf
Marcos Ricardo <mricardoalves99@gmail.com>
2021-09-10 08:11:03 -0300

    feat: setting up external source - google fonts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,6 @@ import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Hello Next</h1>
+    <h1 className={styles.title}>Hello <span>Next</span></h1>
   )
 }
