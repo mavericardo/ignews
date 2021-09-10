f99a867ed3f302ae4d3e173431abd7e92af07c78
Marcos Ricardo <mricardoalves99@gmail.com>
2021-09-10 09:50:09 -0300

    feat: global app styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
-import styles from '../styles/home.module.scss';
+import Head from 'next/head';
+
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Hello <span>Next</span></h1>
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1>Hello Next</h1>
+    </>
   )
 }
