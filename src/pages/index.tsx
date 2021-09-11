e74f39de2bca36f9302e58545003f53a276c1310
Marcos Ricardo <mricardoalves99@gmail.com>
2021-09-11 20:15:01 -0300

    feat: styling home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,26 @@
 import Head from 'next/head';
 
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Hello Next</h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about <br /> the <span>React</span> world</h1>
+        </section>
+
+        <p>
+          Get access to all the publications <br />
+          <span>for $9,90 month</span>
+        </p>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
