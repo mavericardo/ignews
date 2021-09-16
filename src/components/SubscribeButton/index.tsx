45703c981969c283bd33ed84cac18770e25ae242
Marcos Ricardo <mricardoalves99@gmail.com>
2021-09-16 15:38:18 -0300

    feat: consuming stripe API

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,8 +1,12 @@
 import styles from './styles.module.scss';
 
-export function SubscribeButton() {
+interface SubscribeButtonProps {
+    priceId: string;
+}
+
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
     return (
-        <button 
+        <button
             type="button"
             className={styles.subscribeButton}
         >
