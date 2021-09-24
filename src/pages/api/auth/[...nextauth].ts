0db909cb40bcea4d9369eb32523f809fbd0814b9
Marcos Ricardo <mricardoalves99@gmail.com>
2021-09-24 18:40:33 -0300

    feat: saving user in database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query } from 'faunadb';
+
 import NextAuth from "next-auth"
 import Providers from "next-auth/providers"
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
     providers: [
@@ -8,5 +12,27 @@ export default NextAuth({
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
             scope: 'read:user'
         })
-    ]
+    ],
+    jwt: {
+        signingKey: process.env.SIGNING_KEY
+    },
+    callbacks: {
+        async signIn(user, account, profile) {
+            const { email } = user;
+
+            try {
+                await fauna.query(
+                    query.Create(
+                        query.Collection('users'),
+                        { data: { email } }
+                    )
+                );
+
+                return true;
+            } catch {
+                return false;
+            }
+
+        }
+    }
 })
